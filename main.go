cd7368adaa9f3c3788ea79d56038cc773499d024
tanush <tanush@myyahoo.com>
2024-05-18 21:20:38 +0530

    kaka added

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+
+	"firebase.google.com/go/messaging"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tanush-128/openzo_backend/notification/config"
@@ -11,6 +16,7 @@ import (
 	"github.com/tanush-128/openzo_backend/notification/internal/pb"
 	"github.com/tanush-128/openzo_backend/notification/internal/repository"
 	"github.com/tanush-128/openzo_backend/notification/internal/service"
+	"github.com/tanush-128/openzo_backend/notification/internal/utils"
 	"google.golang.org/grpc"
 )
 
@@ -34,10 +40,7 @@ func main() {
 	notificationRepository := repository.NewLocalNotificationRepository(db)
 	notificationService := service.NewLocalNotificationService(notificationRepository)
 
-	// // Initialize gRPC server
-	// grpcServer := grpc.NewServer()
-	// notificationpb.RegisternotificationServiceServer(grpcServer, service.NewGrpcnotificationService(notificationRepository, notificationService))
-	// reflection.Register(grpcServer) // Optional for server reflection
+	go consumeKafka()
 
 	//Initialize gRPC client
 	conn, err := grpc.Dial(cfg.UserGrpc, grpc.WithInsecure())
@@ -70,5 +73,59 @@ func main() {
 	// router.Use(middlewares.JwtMiddleware)
 
 	router.Run(fmt.Sprintf(":%s", cfg.HTTPPort))
+
+}
+
+type Notification struct {
+	Message  string `json:"message"`
+	FCMToken string `json:"fcm_token"`
+}
+
+func consumeKafka() {
+	conf := ReadConfig()
+
+	topic := "notification"
+
+	// sets the consumer group ID and offset
+	conf["group.id"] = "go-group-1"
+	conf["auto.offset.reset"] = "earliest"
+
+	// creates a new consumer and subscribes to your topic
+	consumer, _ := kafka.NewConsumer(&conf)
+	consumer.SubscribeTopics([]string{topic}, nil)
+	var notification Notification
+	run := true
+	for run {
+		// consumes messages from the subscribed topic and prints them to the console
+		e := consumer.Poll(1000)
+		switch ev := e.(type) {
+		case *kafka.Message:
+			// application-specific processing
+			err := json.Unmarshal(ev.Value, &notification)
+			if err != nil {
+				fmt.Println("Error unmarshalling JSON: ", err)
+			}
+			fmt.Println("Sending notification", notification)
+
+			err = utils.SendNotification(&messaging.Message{
+				Notification: &messaging.Notification{
+					Title: "New Order!",
+					Body:  notification.Message,
+				},
+				Token: notification.FCMToken,
+			})
+
+			if err != nil {
+				fmt.Println("Error sending notification: ", err)
+			}
+
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", ev)
+			run = false
+		}
+	}
+
+	// closes the consumer connection
+	consumer.Close()
 
 }
